e4c78e5b8c7c0680406d266758862c0d4c74808c
Huttner David <huttnerd@gmail.com>
2024-01-02 16:29:09 +0100

    Login page implemented (still needs testing)

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,4 +1,34 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 const Login = ({setLogin}) => {
-    return(<div></div>)
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const user = {Email: email, Password: password};
+
+        try{
+            const response = await axios.post("http://localhost:5279/Auth/Login", user);
+            if(response.status === 200){
+                navigate("/");
+                setLogin(true);
+                axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
+            }
+        }
+        catch(error){
+            alert("Registration unsuccesfull!");
+        }
+    }
+
+    return(
+    <div>
+        <input type="text" placeholder="email" onChange={(e)=>{setEmail(e.target.value)}}/>
+        <input type="password" placeholder="password" onChange={(e)=>{setPassword(e.target.value)}}/>
+        <input type="button" onClick={handleSubmit} value={"Submit"}/>
+    </div>)
 }
 export default Login;
